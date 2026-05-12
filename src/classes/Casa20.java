2b90091f51919fc945fcc6e6624e1d703e034beb
gabrielle-sudo <gabriellemartins88021@gmail.com>
2026-05-11 21:59:59 -0300

    Update player entry logic in Casa20
    Add player to the list and update player's position.

--- src/classes/Casa20.java
+++ src/classes/Casa20.java
@@ -7,7 +7,8 @@ public class Casa20 extends Casa {
 
 	public int entrarNaCasa(Jogador jogador) {
 		if (!jogadores.contains(jogador)) {
-			jogadores.add(jogador);
+			jogadores.add(jogador);//se eu vou alterar o jogador a última coisa a fazer é adicioná-lo na lista
+            jogador.setPosicao(this.id);
 		}
 		/*
 		 * casas mágicas: se o competidor parar em uma delas, ele troca de lugar com o
