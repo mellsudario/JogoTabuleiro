ebdb49ced45377000100c01dc07dc9b399df9de3
gabrielle-sudo <gabriellemartins88021@gmail.com>
2026-05-11 21:57:42 -0300

    Update player position when entering Casa10

--- src/classes/Casa10.java
+++ src/classes/Casa10.java
@@ -9,6 +9,7 @@ public class Casa10 extends Casa {
     public void entrarNaCasa(Jogador jogador){
         if(!jogadores.contains(jogador)){
             jogadores.add(jogador);
+            jogador.setPosicao(this.id);
         }
         /*se o competidor parar em uma dessas 
         casa, ele não joga a próxima rodada; */
